aa143e7e4e51047ed6d768d78b7dc7925efc9f30
Taha Tehrani Nasab <79411820+tahatehran@users.noreply.github.com>
2024-08-05 21:30:17 +0330

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -16,10 +16,11 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
 
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
-
-    from app import routes, models, errors
 
     if not app.debug and not app.testing:
         if app.config['LOG_TO_STDOUT']:
@@ -29,9 +30,11 @@ def create_app(config_class=Config):
         else:
             if not os.path.exists('logs'):
                 os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/crypto_dashboard.log', maxBytes=10240, backupCount=10)
+            file_handler = RotatingFileHandler('logs/crypto_dashboard.log', 
+                                               maxBytes=10240, backupCount=10)
             file_handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
             file_handler.setLevel(logging.INFO)
             app.logger.addHandler(file_handler)
 
