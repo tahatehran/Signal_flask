a8a9bc00cda1a0393c9c937b9753bfaf96178fc9
Taha Tehrani Nasab <79411820+tahatehran@users.noreply.github.com>
2024-08-05 20:55:13 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    COINMARKETCAP_API_KEY = os.environ.get('COINMARKETCAP_API_KEY') or 'YOUR_API_KEY'
+    COINMARKETCAP_API_KEY = os.environ.get('COINMARKETCAP_API_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT') == '1'
