549a9a67b32c8d963a7b5e4427c2ff5e9ee54aa7
Taha Tehrani Nasab <79411820+tahatehran@users.noreply.github.com>
2024-08-05 20:13:12 +0330

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    from app import routes, models
+
+    return app
